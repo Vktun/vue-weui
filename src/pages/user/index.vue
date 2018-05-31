dfe178b24e9d16450e3278def38b8a8838044a49
vktun <gxzh0817@hotmail.com>
2018-06-01 01:12:15 +0800

    qiniu sdk

--- src/pages/user/index.vue
+++ src/pages/user/index.vue
@@ -1,30 +1,66 @@
 <template>
   <div class="page__bd">
       <div class="weui-cells">
-            <a class="weui-cell weui-cell_access" href="javascript:;">
-                <div class="weui-cell__hd"><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAC4AAAAuCAMAAABgZ9sFAAAAVFBMVEXx8fHMzMzr6+vn5+fv7+/t7e3d3d2+vr7W1tbHx8eysrKdnZ3p6enk5OTR0dG7u7u3t7ejo6PY2Njh4eHf39/T09PExMSvr6+goKCqqqqnp6e4uLgcLY/OAAAAnklEQVRIx+3RSRLDIAxE0QYhAbGZPNu5/z0zrXHiqiz5W72FqhqtVuuXAl3iOV7iPV/iSsAqZa9BS7YOmMXnNNX4TWGxRMn3R6SxRNgy0bzXOW8EBO8SAClsPdB3psqlvG+Lw7ONXg/pTld52BjgSSkA3PV2OOemjIDcZQWgVvONw60q7sIpR38EnHPSMDQ4MjDjLPozhAkGrVbr/z0ANjAF4AcbXmYAAAAASUVORK5CYII=" alt="" style="width:20px;margin-right:5px;display:block"></div>
-                <div class="weui-cell__bd">
-                    <p>cell standard</p>
-                </div>
-                <div class="weui-cell__ft">说明文字</div>
-            </a>
-            <a class="weui-cell weui-cell_access" href="javascript:;">
-                <div class="weui-cell__hd"><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAC4AAAAuCAMAAABgZ9sFAAAAVFBMVEXx8fHMzMzr6+vn5+fv7+/t7e3d3d2+vr7W1tbHx8eysrKdnZ3p6enk5OTR0dG7u7u3t7ejo6PY2Njh4eHf39/T09PExMSvr6+goKCqqqqnp6e4uLgcLY/OAAAAnklEQVRIx+3RSRLDIAxE0QYhAbGZPNu5/z0zrXHiqiz5W72FqhqtVuuXAl3iOV7iPV/iSsAqZa9BS7YOmMXnNNX4TWGxRMn3R6SxRNgy0bzXOW8EBO8SAClsPdB3psqlvG+Lw7ONXg/pTld52BjgSSkA3PV2OOemjIDcZQWgVvONw60q7sIpR38EnHPSMDQ4MjDjLPozhAkGrVbr/z0ANjAF4AcbXmYAAAAASUVORK5CYII=" alt="" style="width:20px;margin-right:5px;display:block"></div>
-                <div class="weui-cell__bd">
-                    <p>cell standard</p>
-                </div>
-                <div class="weui-cell__ft">说明文字</div>
-            </a>
-        </div>
-    </div>
+           <button class="weui-btn weui-btn_midum weui-btn_primary" @click="upload">+</button>
+     </div>
+  </div>
 </template>
 
 <script>
+import qiniuUploader from "@/utils/qiniuload.js";
 export default {
-
-}
+  data() {
+    return {};
+  },
+  mounted() {},
+  methods: {
+    upload() {
+      const _this = this;
+      wx.request({
+        url: "test.php", // 仅为示例，并非真实的接口地址
+        data: {
+          x: "",
+          y: ""
+        },
+        header: {
+          "content-type": "application/json" // 默认值
+        },
+        success: function(response) {
+          wx.chooseImage({
+            success: function(resp) {
+              const tempFilePaths = resp.tempFilePaths;
+              qiniuUploader.upload(
+                tempFilePaths,
+                res => {
+                  _this.setData({
+                    imageURL: res.imageURL
+                  });
+                },
+                error => {
+                  console.log("error: " + error);
+                },
+                {
+                  region: "ECN",
+                  domain: response.domain,
+                  uptoken: response.token
+                },
+                res => {
+                  console.log("上传进度", res.progress);
+                  console.log("已经上传的数据长度", res.totalBytesSent);
+                  console.log(
+                    "预期需要上传的数据总长度",
+                    res.totalBytesExpectedToSend
+                  );
+                }
+              );
+            }
+          });
+        }
+      });
+    }
+  }
+};
 </script>
 
 <style>
-
 </style>
